ab4c59eef73381830ce95d4e8591b20526992436
ryanc2811 <61228340+ryanc2811@users.noreply.github.com>
2021-01-27 12:56:49 +0000

    Worksheet 2

---
+++ Yr3/Worksheet2/COMP3304Session1/COMP3304Session1/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace COMP3304Session1
+{
+    class Controller
+    {
+        public Controller()
+        {
+            IList<Form> noteForms = new List<Form>();
+
+            Application.Run(new FishyNotes(noteForms));
+        }
+    }
+}
---
+++ Yr3/Worksheet2/COMP3304Session1/COMP3304Session1/FishyNotes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP3304Session1
+{
+    public partial class FishyNotes : Form
+    {
+        //DECLARE An IList of type form, named notes, for storing a reference to each form
+        IList<Form> notes;
+        /// <summary>
+        /// Fish Notes Constructor
+        /// </summary>
+        public FishyNotes(IList<Form>pNotes)
+        {
+            InitializeComponent();
+            notes = pNotes;
+        }
+
+        private void OnNoteDestroyed(object sender, EventArgs e)
+        {
+            // newNote = null;
+            ((Form)sender).Disposed -= OnNoteDestroyed;
+            notes.Remove((Form)sender);
+            Console.WriteLine(notes);
+        }
+        /// <summary>
+        /// Adds a new note on button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddNoteButton_Click(object sender, EventArgs e)
+        {
+            //CREATE new instance of Form
+            Form newNote = new FishyNote();
+            notes.Add(newNote);
+
+            //SUBSCRIBE to dispose event
+            newNote.Disposed += OnNoteDestroyed;
+
+            //SHOW the new note form
+            newNote.Show();
+        }
+    }
+}
